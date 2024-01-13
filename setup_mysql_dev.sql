664b377285dda54eda27bc8c9a99be901ea0bf10
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-13 21:34:28 +0100

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
---check is the database exist
+-- check is the database exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- check if the user exist
-CREATE USER IF NOT EXISTS 'hhnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant select previleges 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hhnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- select PRIVILEGES
-GRANT SELECT ON `performance_schema`.* TO 'hhnb_dev'@'localhost';
---apply changes
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- apply changes
 FLUSH PRIVILEGES;
