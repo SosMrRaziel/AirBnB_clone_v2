905f9ff3d5b90075ce7818f2e82204dcf8c17bce
Emran <ouakouriimran@gmail.com>
2024-01-15 15:18:36 +0100

    task 10

--- models/place.py
+++ models/place.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
-from os import getenv
-from models.review import Review
-from models.amenity import Amenity
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
+from models.review import Review
 
-place_amenity = Table("place_amenity", Base.metadata,
-        Column("place_id", String(60), ForeignKey("places.id"),
-            primary_key=True, nullable=False),
-        Column("amenity_id", String(60), ForeignKey("amenities.id"),
-            primary_key=True, nullable=False))
+
+place_amenity = Table('place_amenity', Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'), 
+               primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'),
+               primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
@@ -26,6 +26,6 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+    amenity_ids = []
