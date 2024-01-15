8ffe36959409610e949886940e827123dcce5fd4
Emran <ouakouriimran@gmail.com>
2024-01-15 15:36:09 +0100

    task 10

--- models/place.py
+++ models/place.py
@@ -25,5 +25,5 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+#    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
     amenity_ids = []
