cf277e945d491b4319bcf27a5c4fc0a4e468e174
Emran <ouakouriimran@gmail.com>
2024-01-15 15:13:43 +0100

    task 10

--- models/place.py
+++ models/place.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
-
+from models.amenity import Amenity
+from models.review import Review
 place_amenity = Table('place_amenity', Base.metadata,
                         Column('place_id', String(60), ForeignKey('places.id'),
                                primary_key=True, nullable=False),
