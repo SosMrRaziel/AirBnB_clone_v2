53521f5a0860ae2a5fc730ad0165a8fa2d6e1f9b
Emran <ouakouriimran@gmail.com>
2024-01-15 15:11:40 +0100

    task 10

--- models/place.py
+++ models/place.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
-place_amenity = ('place_amenity', Base.metadata,
+place_amenity = Table('place_amenity', Base.metadata,
                         Column('place_id', String(60), ForeignKey('places.id'),
                                primary_key=True, nullable=False),
                         Column('amenity_id', String(60), ForeignKey('amenities.id'),
