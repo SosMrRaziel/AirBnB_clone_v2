eb36d4213f9bdd4a97517db43e36066f30e0df61
Emran <ouakouriimran@gmail.com>
2024-01-15 15:26:25 +0100

    task 10

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-#from models import storage
+from models import storage
 
 Base = declarative_base()
 
@@ -34,7 +34,6 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
