124d02d37f45abbbd1dfc82185b5b1e0ac6809db
Emran <ouakouriimran@gmail.com>
2024-01-15 15:17:29 +0100

    task 10

--- models/place.py
+++ models/place.py
@@ -5,11 +5,13 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from models.amenity import Amenity
 from models.review import Review
+
+
 place_amenity = Table('place_amenity', Base.metadata,
-                        Column('place_id', String(60), ForeignKey('places.id'),
-                               primary_key=True, nullable=False),
-                        Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                               primary_key=True, nullable=False))
+        Column('place_id', String(60), ForeignKey('places.id'), 
+               primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'),
+               primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -25,5 +27,5 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary='place_amenity', viewonly=False)
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
     amenity_ids = []
