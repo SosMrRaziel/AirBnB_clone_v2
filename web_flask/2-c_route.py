5d2e416a8fcfa72e7ddb05636eaead2afe11201e
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-15 17:19:53 +0100

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,5 +11,9 @@ def hello_hbnb():
 def display_hbnb():
     return "“HBNB”"
 
+@app.route("/c/<text>", strict_slashes=False)
+def display_C_(text):
+    return "C {}".format(text.replace("_", " "))
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
