644950fbe12d5ae5de2a85ca117ac11321e03836
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-15 19:10:10 +0100

    task

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 """ Flask app that return hello HBNB"""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -26,6 +26,10 @@ def display_Python_(text=""):
 @app.route("/number/<int:n>", strict_slashes=False)
 def display_num(n):
     return "{} is a number".format(n)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_num_template(n):
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
