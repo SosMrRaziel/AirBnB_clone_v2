6ad0568130bccb2a2b4da6d397456423731f8ec9
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-16 17:19:09 +0100

    task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python3
 """ Flask app that return hello HBNB"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    return "“Hello HBNB!”"
+def hello():
+    """ say hello hbnb """
+    return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
+    """ desplay hbnb"""
     return "“HBNB”"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
 """ Flask app that return hello HBNB"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    return "“Hello HBNB!”"
+def hello():
+    """ say hello hbnb """
+    return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
