6cc225eb781d7ced20571bdfabc221bcd5874044
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-16 17:38:33 +0100

    task

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,34 +1,47 @@
+#!/usr/bin/python3
 """ Flask app that return hello HBNB"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    return "“Hello HBNB!”"
+def hello():
+    """ say hello hbnb """
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
-    return "“HBNB”"
+    """ desplay hbnb"""
+    return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def display_C_(text):
+    """ desplay ur text """
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def display_Python_(text=""):
+    """ desplay python is $"""
     if text == "":
         return "Python is cool"
     else:
         return "Python {}".format(text.replace("_", " "))
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def display_num(n):
+    """ desplay only numbers """
     return "{} is a number".format(n)
+
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def display_num_template(n):
+    """ rednder numbers on html """
     return render_template("5-number.html", n=n)
 
 
