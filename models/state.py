8aae3af5a870cc0c191b4fd631d7a1d28a44051b
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-16 20:09:36 +0100

    task

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 import models
+from models.city import City
 
 
 class State(BaseModel, Base):
