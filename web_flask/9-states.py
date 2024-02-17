5c5450ca35b9272e44ad93a1a976ca34c6c7bfb9
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-17 19:40:14 +0100

    task

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-from flask import Flask, render_template
+"""a script that starts a Flask web application"""
 from models import storage
+from flask import Flask, render_template
 from models.state import State
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/states')
-@app.route('/states/<id>')
-def states(id=None):
-    """Display a HTML page with the list of states or a specific state"""
-    states = storage.all(State)
-    if id:
-        id = "State." + id
-    return render_template('9-states.html', states=states, id=id)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
+def session_close(exception):
+    """a method to terminate a session"""
     storage.close()
 
 
+@app.route("/states/<id>", strict_slashes=False)
+def display_by_id(id):
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', state=state, id=id)
+    return render_template('9-states.html')
+
+
+@app.route("/states", strict_slashes=False)
+def display():
+    """a method to display an html file"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
